a0a1b5120d713586e074eb933657efae5d73efb2
Donnie <Donnie@DESKTOP-A0TIJCR>
2016-10-02 10:40:08 +0200

    Add project files.

---
+++ TicTacToe.UnitTests/GameWinnerServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TicTacToe.UnitTests
+{
+    [TestFixture]
+    class GameWinnerServiceTests
+    {
+        [Test]
+        public void NeitherPlayerHasThreeInARow()
+        {
+            const char expected = ' ';
+            var gameBoard = new char[3, 3] { {' ', ' ',' '},
+                                             {' ', ' ',' '},
+                                             {' ',' ',' '}};
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
